c725d7ce04b8b0ac216df0d7b31bfef33cbdf126
= <sinulinggatwo@gmail.com>
2021-04-05 23:26:44 +0700

    add package db.

---
+++ db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"os"
+	"fmt"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	right := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
+	db, err := sql.Open(os.Getenv("DB_DRIVER"), right)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
