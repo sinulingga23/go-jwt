bbfb211ff2118ea951a8d106f4a092d8a32f2740
= <sinulinggatwo@gmail.com>
2021-04-06 22:05:10 +0700

    add the methods for category

--- model/category_model.go
+++ model/category_model.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"log"
+	"errors"
+
 	database "github.com/sinulingga23/go-jwt/db"
 )
 
@@ -10,6 +12,28 @@ type Category struct {
 	Category	string	`json:"category"`
 	Audit		Audit	`json:"audit"`
 }
+
+
+func (c *Category) IsCategoryExistByCategoryId(categoryId int) (bool, error) {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	var check int64 = 0
+	err = db.QueryRow("SELECT COUNT(category_id) FROM category WHERE category_id = ?", categoryId).Scan(&check)
+	if err != nil {
+		return false, err
+	}
+
+	if check != 1 {
+		return false, errors.New("Somethings wrong!")
+	}
+
+	return true, nil
+}
+
 
 func (c *Category) SaveCategory() (Category, error) {
 	db, err := database.ConnectDB()
@@ -35,4 +59,122 @@ func (c *Category) SaveCategory() (Category, error) {
 	var category Category
 	category = Category{CategoryId: c.CategoryId, Category: c.Category, Audit: Audit {CreatedAt: c.Audit.CreatedAt}}
 	return category, nil
+}
+
+func (c *Category) FindCategoryByCategoryId(categoryId int) (Category, error) {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return Category{}, err
+	}
+	defer db.Close()
+
+	var category Category
+	err = db.QueryRow("SELECT category_id, category, created_at, updated_at FROM category WHERE category_id = ?", categoryId).
+		Scan(&category.CategoryId, &category.Category, &category.Audit.CreatedAt, &category.Audit.UpdatedAt)
+	if err != nil {
+		return Category{}, err
+	}
+
+	if category == (Category{}) {
+		return Category{}, errors.New("Category can't found")
+	}
+
+	return category, nil
+}
+
+func (c *Category) UpdateCategoryByCategoryId(categoryId int) (Category, error) {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return Category{}, err
+	}
+	defer db.Close()
+
+	result, err := db.Exec("UPDATE category SET category_id = ?, category = ?, created_at = ?, updated_at = ? WHERE category_id = ?",
+		c.CategoryId,
+		c.Category,
+		c.Audit.CreatedAt,
+		c.Audit.UpdatedAt,
+		categoryId)
+	if err != nil {
+		return Category{}, err
+	}
+
+	var rowsAffected int64
+	if rowsAffected, err = result.RowsAffected(); err != nil {
+		return Category{}, err
+	}
+
+	if rowsAffected == 0 {
+		return Category{}, errors.New("Maybe the category is not exist.")
+	}
+
+	if rowsAffected != 1 {
+		return Category{}, errors.New("Somethings wrong!")
+	}
+
+	var category Category
+	category = Category{
+			CategoryId: c.CategoryId,
+			Category: c.Category,
+			Audit: Audit {CreatedAt: c.Audit.CreatedAt, UpdatedAt: c.Audit.UpdatedAt},}
+	return category, nil
+}
+
+func (c *Category) DeleteCategoryByCategoryId(categoryId int) (bool, error) {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	result, err := db.Exec("DELETE FROM category WHERE category_id = ?", categoryId)
+	if err != nil {
+		return false, err
+	}
+
+	var rowsAffected int64
+	if rowsAffected, err = result.RowsAffected(); err != nil {
+		return false, err
+	}
+
+	if rowsAffected == 0 {
+		return false, errors.New("Maybe the category is not exist.")
+	}
+
+	if rowsAffected != 1 {
+		return false, errors.New("Somethings wrong")
+	}
+
+	return true, nil
+}
+
+func (c *Category) FindProductsByCategoryId(categoryId int) ([]Product, error) {
+	db, err := database.ConnectDB()
+	if err != nil {
+		return []Product{}, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT p.product_id, p.category_id, p.name, p.unit, p.price, p.stock, p.created_at, p.updated_at FROM product p INNER JOIN category c ON p.product_id = c.category_id HAVING p.category_id = ?", categoryId)
+	if err != nil {
+		return []Product{}, err
+	}
+	defer rows.Close()
+
+	var result []Product
+	for rows.Next() {
+		var each Product
+		err = rows.Scan(&each.ProductId, &each.CategoryId, &each.Name, &each.Unit, &each.Price, &each.Stock, &each.Audit.CreatedAt, &each.Audit.UpdatedAt)
+		if err != nil {
+			return []Product{}, err
+		}
+
+		result = append(result, each)
+	}
+
+	if err = rows.Err(); err != nil {
+		return []Product{}, err
+	}
+
+	return result, nil
 }
