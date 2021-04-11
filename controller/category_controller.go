44e367b109310e2899c9bc9785e43e59cde2ca04
= <sinulinggatwo@gmail.com>
2021-04-11 23:05:35 +0700

    fill the get categories controller

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -1,11 +1,47 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"encoding/json"
+
+	"github.com/sinulingga23/go-jwt/model"
 )
 
+
+
 var GetCategories = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GetCategories"))
+	w.Header().Set("Content-Type", "application/json")
+
+	var categories []model.Category
+	var categoryModel model.Category
+	var err error
+
+	if categories, err = categoryModel.GetAllCategory(); err != nil {
+		payload, _ := json.Marshal(struct {
+			StatusCode	int		`json:"statusCode"`
+			Message		string		`json:"message"`
+			Errors		string		`json:"errors"`
+		}{
+			http.StatusNotFound,
+			"Somethings wrong!",
+			fmt.Sprintf("%s", err),
+		})
+		w.Write([]byte(payload))
+		return
+	}
+
+	payload, _ := json.Marshal(struct {
+		StatusCode	int			`json:"statusCode"`
+		Message		string			`json:"message"`
+		Data		[]model.Category	`json:"categories"`
+	}{
+		http.StatusOK,
+		"Success to get categories",
+		categories,
+	})
+	w.Write([]byte(payload))
+	return
 })
 
 var CreateCategory = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
