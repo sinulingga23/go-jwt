7cfe36fc4c4f5f40fc434c4fd634c9c3034235d9
= <sinulinggatwo@gmail.com>
2021-04-12 22:14:08 +0700

    fill the create category handler

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"time"
+	"strings"
 	"net/http"
 	"encoding/json"
 
@@ -45,7 +47,58 @@ var GetCategories = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request
 })
 
 var CreateCategory = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("CreateCategory"))
+	w.Header().Set("Content-Type", "application/json")
+
+	var categoryRequest model.Category
+	var err error
+
+	if err = json.NewDecoder(r.Body).Decode(&categoryRequest); err != nil {
+		payload, _ := json.Marshal(struct {
+			StatusCode	int	`json:"statusCode"`
+			Message		string	`json:"message"`
+			Errors		string	`json:"errors"`
+		}{
+			http.StatusBadRequest, "invalid", fmt.Sprintf("%s", err),
+		})
+		w.Write([]byte(payload))
+		return
+	}
+
+	if len(strings.Trim(categoryRequest.Category, " ")) == 0 {
+		payload, _ := json.Marshal(struct {
+			StatusCode	int	`json:"statusCode"`
+			Message		string	`json:"message"`
+			Errors		string	`json:"errors"`
+		}{
+			http.StatusBadRequest, "Category name can't be empty.", "BadRequest",
+		})
+		w.Write([]byte(payload))
+		return
+	}
+
+	var createdCategory model.Category
+	categoryRequest.Audit.CreatedAt = time.Now().Format("2006-01-02 15:05:03")
+	if createdCategory, err = categoryRequest.SaveCategory(); err != nil {
+		payload, _ := json.Marshal(struct {
+			StatusCode	int	`json:"statusCode"`
+			Message		string 	`json:"message"`
+			Errors		string	`json:"errors"`
+		}{
+			http.StatusInternalServerError, "Somethings wrong!", fmt.Sprintf("%s", err),
+		})
+		w.Write([]byte(payload))
+		return
+	}
+
+	payload, _ := json.Marshal(struct {
+		StatusCode	int 		`json:"statusCode"`
+		Message		string		`json:"message"`
+		Data		model.Category	`json:"category"`
+	}{
+		http.StatusOK, "success to create a new category", createdCategory,
+	})
+	w.Write([]byte(payload))
+	return
 })
 
 var GetCategoryByCategoryId = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
