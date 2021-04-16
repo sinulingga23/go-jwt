ab609c50f9c180c57502fcf10d21cf1fed8ab08c
= <sinulinggatwo@gmail.com>
2021-04-16 22:44:09 +0700

    fill the get products handler

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,11 +1,41 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"encoding/json"
+
+	"github.com/sinulingga23/go-jwt/model"
+	_"github.com/gorilla/mux"
 )
 
 var GetProducts = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GetProducts"))
+	w.Header().Set("Content-Type", "application/json")
+
+	var productModel model.Product
+	var products []model.Product
+	var err error
+	if products, err = productModel.FindAllProduct(); err != nil {
+		payload, _ := json.Marshal(struct {
+			StatusCode	int 	`json:"statusCode"`
+			Message 	string 	`json:"message"`
+			Errors 		string	`json:"errors"`
+		}{
+			http.StatusBadRequest, "Somethings wrong!", fmt.Sprintf("%s", err),
+		})
+		w.Write([]byte(payload))
+		return
+	}
+
+	payload, _ := json.Marshal(struct {
+		StatusCode	int		`json:"statusCode"`
+		Message 	string 		`json:"message"`
+		Data 		[]model.Product	`json:"products"`
+	}{
+		http.StatusOK, "Success to get the products", products,
+	})
+	w.Write([]byte(payload))
+	return
 })
 
 var CreateProduct = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
