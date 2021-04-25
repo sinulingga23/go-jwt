63b89af95316a2057af0ad2be80d9b0939b14a3e
= <sinulinggatwo@gmail.com>
2021-04-25 15:31:04 +0700

    add middleware to requests POST, PUT, and DELETE

--- controller/route.go
+++ controller/route.go
@@ -2,26 +2,30 @@ package controller
 
 import (
 	"net/http"
+	"github.com/sinulingga23/go-jwt/middleware"
 	"github.com/gorilla/mux"
 )
 
 func RunServer() {
 	router := mux.NewRouter()
 
+	// endpoints for authtenticaiton & authorization purpose
+	router.Handle("/login", LoginUser).Methods("POST")
+
 	// endpoints categories
 	router.Handle("/categories", GetCategories).Methods("GET")
-	router.Handle("/categories", CreateCategory).Methods("POST")
+	router.Handle("/categories", middleware.CheckAuthenticationMiddleware(CreateCategory)).Methods("POST")
 	router.Handle("/categories/{categoryId}", GetCategoryByCategoryId).Methods("GET")
-	router.Handle("/categories/{categoryId}", UpdateCategoryByCategoryId).Methods("PUT")
-	router.Handle("/categories/{categoryId}", DeleteCategoryByCategoryId).Methods("DELETE")
+	router.Handle("/categories/{categoryId}", middleware.CheckAuthenticationMiddleware(UpdateCategoryByCategoryId)).Methods("PUT")
+	router.Handle("/categories/{categoryId}", middleware.CheckAuthenticationMiddleware(DeleteCategoryByCategoryId)).Methods("DELETE")
 	router.Handle("/categories/{categoryId}/products", GetProductsByCategoryId).Methods("GET")
 
 	// endpoints products
 	router.Handle("/products", GetProducts).Methods("GET")
-	router.Handle("/products", CreateProduct).Methods("POST")
+	router.Handle("/products", middleware.CheckAuthenticationMiddleware(CreateProduct)).Methods("POST")
 	router.Handle("/products/{productId}", GetProductByProductId).Methods("GET")
-	router.Handle("/products/{productId}", UpdateProductByProductId).Methods("PUT")
-	router.Handle("/products/{productId}", DeleteProductByProductId).Methods("DELETE")
+	router.Handle("/products/{productId}", middleware.CheckAuthenticationMiddleware(UpdateProductByProductId)).Methods("PUT")
+	router.Handle("/products/{productId}", middleware.CheckAuthenticationMiddleware(DeleteProductByProductId)).Methods("DELETE")
 
 	http.ListenAndServe(":8080", router)
 }
